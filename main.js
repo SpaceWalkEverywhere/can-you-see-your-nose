5eb850f65eb951a98ac81b3b64325dabe3983229
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-06-22 15:34:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,28 @@ function preload() {
 
 }
 function setup(){
-    Canvas=createCanvas(300,300);
+    Canvas=createCanvas(375,300);
     Canvas.center();
+    cam=createCapture(VIDEO);
+    cam.size(375,300);
+    cam.hide();
+    net=ml5.poseNet(cam,modelloaded)
+    net.on('pose',gotposes)
 }
 function draw(){
+    image(cam,0,0,375,300);
     
 }
 function ohsnap(){
     save("no_nose.png");
+}
+function modelloaded(){
+    console.log("woohoo !!! your model is loaded.");
+
+}
+function gotposes(results){
+    
+    if(results.length>0){
+        console.log(results);
+    }
 }
